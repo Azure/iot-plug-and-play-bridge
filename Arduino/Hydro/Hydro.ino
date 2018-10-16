666333013f263f154d4f7b7868fbdb845b1f27e4
Alex Scarpantoni <alscarpa@microsoft.com>
2018-10-16 14:52:57 -0700

    Update hydro

--- Arduino/Hydro/Hydro.ino
+++ Arduino/Hydro/Hydro.ino
@@ -27,28 +27,14 @@
 
 #include "SerialPnP.h"
 
-uint16_t g_sampleRate = 1000;
+int32_t g_sampleRate = 1000;
 
 long g_lastSample = 0;
 
-void CbSampleRate(uint16_t *input, uint16_t *output) {
+void CbSampleRate(int32_t *input, int32_t *output) {
   if (output) *output = g_sampleRate;
 
   if (input) g_sampleRate = *input;
-}
-
-void CbTest(uint8_t *input, uint8_t *output) {
-  if (input) {
-    output[0] = 2*input[0];
-    output[1] = 2*input[1];
-    output[2] = 2*input[2];
-    output[3] = 2*input[3];
-  } else {
-    output[0] = 0xFE;
-    output[1] = 0xEF;
-    output[2] = 0xDE;
-    output[3] = 0xED;
-  }
 }
 
 // the setup function runs once when you press reset or power the board
@@ -60,8 +46,8 @@ void setup() {
     pinMode(LIGHT_SWITCH_1, OUTPUT);
     pinMode(PUMP_SWITCH_1, OUTPUT);
 
-    SerialPnP::Setup("xGrow");
-    SerialPnP::NewInterface("http://contoso.com/exampleint");
+    SerialPnP::Setup("Hydro");
+    SerialPnP::NewInterface("http://contoso.com/hydro");
 
     SerialPnP::NewEvent("ambient_light",
                         "Ambient Light",
@@ -88,7 +74,7 @@ void setup() {
                            SerialPnPSchema::SchemaInt,
                            false, // required
                            true, // writeable
-                           (SerialPnPCb*) CbTest); // callback function on update
+                           (SerialPnPCb*) CbSampleRate); // callback function on update
 
     // Initialize I2C connection for our pH and EC sensors
     Wire.begin();
