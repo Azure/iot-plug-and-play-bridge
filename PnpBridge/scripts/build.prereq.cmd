2a10e4081df3a72284169914ed08ff4f4a224cdd
ajbarb <ajaypreetham@gmail.com>
2019-02-01 13:34:34 -0800

    Add ARM config (#2)

--- PnpBridge/scripts/build.prereq.cmd
+++ PnpBridge/scripts/build.prereq.cmd
@@ -4,7 +4,7 @@ goto START
 
 :Usage
 echo Usage: build.prereq.cmd x86^|ARM^|x64 Debug^|Release [WindowsSDKVersion]
-echo    WinSDKVer............... Default is 10.0.14393.0, specify another version if necessary
+echo    WindowsSDKVersion............... Default is 10.0.17763.0, specify another version if necessary
 echo    [/?].................... Displays this usage string.
 echo    Example:
 echo        build.azure-c-sdk.cmd x64 Debug 10.0.17763.0
@@ -52,15 +52,15 @@ set OUTPUTDIR=%TARGETPLATFORM%_%TARGETCONFIG%
 md %OUTPUTDIR%
 pushd %OUTPUTDIR%
 if /I [%TARGETARCH%] == [x86] (
-cmake -G "Visual Studio 15 2017" ..
+cmake -G "Visual Studio 15 2017" .. -Dskip_samples=ON ..
 )
 
 if /I [%TARGETARCH%] == [arm] (
-cmake -G "Visual Studio 15 2017 ARM" .. -Duse_prov_client:BOOL=ON -Duse_tpm_simulator:BOOL=OFF -Dbuild_provisioning_service_client=ON -Duse_prov_client_core=ON -Dskip_samples=ON ..
+cmake -G "Visual Studio 15 2017 ARM" .. -Dskip_samples=ON ..
 )
 
 if /I [%TARGETARCH%] == [x64] (
-cmake -G "Visual Studio 15 2017 Win64" ..
+cmake -G "Visual Studio 15 2017 Win64" .. -Dskip_samples=ON ..
 )
 popd
 
