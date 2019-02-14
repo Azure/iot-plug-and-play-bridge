2c20c91e20db858e2dec6849d89bda4349187109
Ajay Barboza <ajbarb@microsoft.com>
2019-02-14 13:53:38 -0800

    cleanup comments

--- PnpBridge/src/PnpBridge/PnpBridge.h
+++ PnpBridge/src/PnpBridge/PnpBridge.h
@@ -6,48 +6,8 @@
  | _ \_ _  _ __| _ )_ _(_)__| |__ _ ___
  |  _/ ' \| '_ \ _ \ '_| / _` / _` / -_)
  |_| |_||_| .__/___/_| |_\__,_\__, \___|
-		  |_|                 |___/
+          |_|                 |___/
 */
-
-// TODO: REMOVE THE COMMENTS
-// NOTES TO SELF:
-
-//
-// Config update requires bridge tear down and restart
-//
-
-//
-// Discovered Device -> Bind -> CreatePnpInterfaces -> Create Pnp Interface client ->
-// Create Pnp Adapter Interface -> Associate it with the adapter and store the device config pointer -> Get All interface for all adapters -> publish
-//
-
-/*
-
-0. TEST FOR MEMORY LEAKS USING APPVERIFIER
-
-SerialPnp
- 1. Closing serial handle
- 2. Test publishing 2 interfaces (multiple)
- 3. Test overlapped IO
-
-PnpBridge
- 4. DPS connection & test
-
- 5. FT to create a tear down in a loop *** MOCK the PNP CLIENT SDK
- 6. Release PnpInterfaces.. - DONE
- 7. Persistent device should get callback when device is discovered
- 7.1 Use event to notify PnpBridgeWorker thread exit
-
-CoreDevice
- 7. Enumerate devices
- 8. Add properties based on original device schema
-
-CMAKE
- 9. Clean up warnings to get a clean build
- 10. Creating VS filters: https://stackoverflow.com/questions/33808087/cmake-how-to-create-visual-studio-filters
-
-*/
-// ***************
 
 #pragma once
 
