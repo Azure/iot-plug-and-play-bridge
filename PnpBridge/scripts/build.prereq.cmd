369ed5ea573721178c46da5e4814ad5369c451d9
ajbarb <ajaypreetham@gmail.com>
2019-02-14 15:40:48 -0800

    User/ajbarb/update buildscripts (#5)
    * add vs directory filter in cmake
    
    * Fix build break
    
    * update build sripts

--- PnpBridge/scripts/build.prereq.cmd
+++ PnpBridge/scripts/build.prereq.cmd
@@ -4,10 +4,10 @@ goto START
 
 :Usage
 echo Usage: build.prereq.cmd x86^|ARM^|x64 Debug^|Release [WindowsSDKVersion]
-echo    WinSDKVer............... Default is 10.0.14393.0, specify another version if necessary
+echo    WindowsSDKVersion............... Default is 10.0.17763.0, specify another version if necessary
 echo    [/?].................... Displays this usage string.
 echo    Example:
-echo        build.azure-c-sdk.cmd x64 Debug 10.0.17763.0
+echo        build.prereq.cmd x64 Debug WindowsSDKVersion
 endlocal
 exit /b 1
 
@@ -24,7 +24,7 @@ if [%1] == [] (
 )
 
 if /I [%TARGETARCH%] == [x86] (
-	set TARGETPLATFORM=Win32
+    et TARGETPLATFORM=Win32
 ) else (
     set TARGETPLATFORM=%TARGETARCH%
 )
@@ -56,7 +56,7 @@ cmake -G "Visual Studio 15 2017" ..
 )
 
 if /I [%TARGETARCH%] == [arm] (
-cmake -G "Visual Studio 15 2017 ARM" .. -Duse_prov_client:BOOL=ON -Duse_tpm_simulator:BOOL=OFF -Dbuild_provisioning_service_client=ON -Duse_prov_client_core=ON -Dskip_samples=ON ..
+cmake -G "Visual Studio 15 2017 ARM" .. -Dskip_samples=ON ..
 )
 
 if /I [%TARGETARCH%] == [x64] (
