6ce3d685077ae89529acd05f4328f05b46b225ad
Kevin Wenner <kwenner@impinj.com>
2020-11-25 11:20:27 -0800

    Added minimum PnP bridge interface functions, copied from environmental sensor example (as starting point)

--- pnpbridge/src/adapters/src/impinj_reader_r700/impinj_reader_r700.c
+++ pnpbridge/src/adapters/src/impinj_reader_r700/impinj_reader_r700.c
@@ -9,6 +9,129 @@
 #include "azure_c_shared_utility/const_defines.h"
 
 
+IOTHUB_CLIENT_RESULT ImpinjReader_CreatePnpAdapter(
+    const JSON_Object* AdapterGlobalConfig,
+    PNPBRIDGE_ADAPTER_HANDLE AdapterHandle)
+{
+    AZURE_UNREFERENCED_PARAMETER(AdapterGlobalConfig);
+    AZURE_UNREFERENCED_PARAMETER(AdapterHandle);
+    return IOTHUB_CLIENT_OK;
+}
+
+IOTHUB_CLIENT_RESULT
+ImpinjReader_CreatePnpComponent(
+    PNPBRIDGE_ADAPTER_HANDLE AdapterHandle,
+    const char* ComponentName, 
+    const JSON_Object* AdapterComponentConfig,
+    PNPBRIDGE_COMPONENT_HANDLE BridgeComponentHandle)
+{
+    AZURE_UNREFERENCED_PARAMETER(AdapterComponentConfig);
+    AZURE_UNREFERENCED_PARAMETER(AdapterHandle);
+    IOTHUB_CLIENT_RESULT result = IOTHUB_CLIENT_OK;
+    PIMPINJ_READER device = NULL;
+
+    if (strlen(ComponentName) > PNP_MAXIMUM_COMPONENT_LENGTH)
+    {
+        LogError("ComponentName=%s is too long.  Maximum length is=%d", ComponentName, PNP_MAXIMUM_COMPONENT_LENGTH);
+        BridgeComponentHandle = NULL;
+        result = IOTHUB_CLIENT_ERROR;
+        goto exit;
+    }
+
+    device = calloc(1, sizeof(IMPINJ_READER));
+    if (NULL == device) {
+
+        LogError("Unable to allocate memory for Impinj Reader component.");
+        result = IOTHUB_CLIENT_ERROR;
+        goto exit;
+    }
+
+    device->SensorState = calloc(1, sizeof(IMPINJ_READER_STATE));
+    if (NULL == device) {
+        LogError("Unable to allocate memory for Impinj Reader component state.");
+        result = IOTHUB_CLIENT_ERROR;
+        goto exit;
+    }
+
+    mallocAndStrcpy_s(&device->SensorState->componentName, ComponentName);
+
+    PnpComponentHandleSetContext(BridgeComponentHandle, device);
+    PnpComponentHandleSetPropertyUpdateCallback(BridgeComponentHandle, ImpinjReader_ProcessPropertyUpdate);
+    PnpComponentHandleSetCommandCallback(BridgeComponentHandle, ImpinjReader_ProcessCommand);
+
+exit:
+    return result;
+}
+
+IOTHUB_CLIENT_RESULT ImpinjReader_StartPnpComponent(
+    PNPBRIDGE_ADAPTER_HANDLE AdapterHandle,
+    PNPBRIDGE_COMPONENT_HANDLE PnpComponentHandle)
+{
+    IOTHUB_CLIENT_RESULT result = IOTHUB_CLIENT_OK;
+    AZURE_UNREFERENCED_PARAMETER(AdapterHandle);
+    PIMPINJ_READER device = PnpComponentHandleGetContext(PnpComponentHandle);
+
+    // Store client handle before starting Pnp component
+    device->ClientHandle = PnpComponentHandleGetClientHandle(PnpComponentHandle);
+
+    // Set shutdown state
+    device->ShuttingDown = false;
+    LogInfo("Environmental Sensor: Starting Pnp Component");
+
+    PnpComponentHandleSetContext(PnpComponentHandle, device);
+
+    // Report Device State Async
+    result = SampleImpinjReader_ReportDeviceStateAsync(PnpComponentHandle, device->SensorState->componentName);
+
+    // Create a thread to periodically publish telemetry
+    if (ThreadAPI_Create(&device->WorkerHandle, ImpinjReader_TelemetryWorker, PnpComponentHandle) != THREADAPI_OK) {
+        LogError("ThreadAPI_Create failed");
+        return IOTHUB_CLIENT_ERROR;
+    }
+    return IOTHUB_CLIENT_OK;
+}
+
+IOTHUB_CLIENT_RESULT ImpinjReader_StopPnpComponent(
+    PNPBRIDGE_COMPONENT_HANDLE PnpComponentHandle)
+{
+    PIMPINJ_READER device = PnpComponentHandleGetContext(PnpComponentHandle);
+
+    if (device) {
+        device->ShuttingDown = true;
+        ThreadAPI_Join(device->WorkerHandle, NULL);
+    }
+    return IOTHUB_CLIENT_OK;
+}
+
+IOTHUB_CLIENT_RESULT ImpinjReader_DestroyPnpComponent(
+    PNPBRIDGE_COMPONENT_HANDLE PnpComponentHandle)
+{
+    PIMPINJ_READER device = PnpComponentHandleGetContext(PnpComponentHandle);
+    if (device != NULL)
+    {
+        if (device->SensorState != NULL)
+        {
+            if (device->SensorState->customerName != NULL)
+            {
+                free(device->SensorState->customerName);
+            }
+            free(device->SensorState);
+        }
+        free(device);
+
+        PnpComponentHandleSetContext(PnpComponentHandle, NULL);
+    }
+
+    return IOTHUB_CLIENT_OK;
+}
+
+
+IOTHUB_CLIENT_RESULT ImpinjReader_DestroyPnpAdapter(
+    PNPBRIDGE_ADAPTER_HANDLE AdapterHandle)
+{
+    AZURE_UNREFERENCED_PARAMETER(AdapterHandle);
+    return IOTHUB_CLIENT_OK;
+}
 
 PNP_ADAPTER ImpinjReaderR700 = {
     .identity = "impinj-reader-r700",
--- pnpbridge/src/adapters/src/impinj_reader_r700/impinj_reader_r700.h
+++ pnpbridge/src/adapters/src/impinj_reader_r700/impinj_reader_r700.h
@@ -7,6 +7,7 @@
 extern "C"
 {
 #endif
+
 #include <pnpadapter_api.h>
 
 #include "azure_c_shared_utility/azure_base32.h"
@@ -19,6 +20,69 @@ extern "C"
 
 
 #include "parson.h"
+
+
+//
+// Application state associated with the particular component. In particular it contains 
+// the resources used for responses in callbacks along with properties set
+// and representations of the property update and command callbacks invoked on given component
+//
+typedef struct IMPINJ_READER_STATE_TAG
+{
+    char * componentName;
+    int brightness;
+    char* customerName;
+    int numTimesBlinkCommandCalled;
+    int blinkInterval;
+} IMPINJ_READER_STATE, * PIMPINJ_READER_STATE;
+
+typedef struct _IMPINJ_READER {
+    THREAD_HANDLE WorkerHandle;
+    volatile bool ShuttingDown;
+    PIMPINJ_READER_STATE SensorState;
+    PNP_BRIDGE_CLIENT_HANDLE ClientHandle;
+} IMPINJ_READER, * PIMPINJ_READER;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    // Sends  telemetry messages about current environment
+    IOTHUB_CLIENT_RESULT ImpinjReader_SendTelemetryMessagesAsync(
+        PNPBRIDGE_COMPONENT_HANDLE PnpComponentHandle);
+    IOTHUB_CLIENT_RESULT ImpinjReader_ReportDeviceStateAsync(
+        PNPBRIDGE_COMPONENT_HANDLE PnpComponentHandle,
+        const char * ComponentName);
+    IOTHUB_CLIENT_RESULT ImpinjReader_RouteReportedState(
+        void * ClientHandle,
+        PNPBRIDGE_COMPONENT_HANDLE PnpComponentHandle,
+        const unsigned char * ReportedState,
+        size_t Size,
+        IOTHUB_CLIENT_REPORTED_STATE_CALLBACK ReportedStateCallback,
+        void * UserContextCallback);
+    IOTHUB_CLIENT_RESULT ImpinjReader_RouteSendEventAsync(
+        PNPBRIDGE_COMPONENT_HANDLE PnpComponentHandle,
+        IOTHUB_MESSAGE_HANDLE EventMessageHandle,
+        IOTHUB_CLIENT_EVENT_CONFIRMATION_CALLBACK EventConfirmationCallback,
+        void * UserContextCallback
+        );
+    void ImpinjReader_ProcessPropertyUpdate(
+        void * ClientHandle,
+        const char* PropertyName,
+        JSON_Value* PropertyValue,
+        int version,
+        PNPBRIDGE_COMPONENT_HANDLE PnpComponentHandle);
+    int ImpinjReader_ProcessCommandUpdate(
+        PIMPINJ_READER EnvironmentalSensor,
+        const char* CommandName,
+        JSON_Value* CommandValue,
+        unsigned char** CommandResponse,
+        size_t* CommandResponseSize);
+
+#ifdef __cplusplus
+}
+#endif
 
 #ifdef __cplusplus
 }
