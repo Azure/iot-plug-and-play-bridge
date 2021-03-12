3ff70f12cf75359901667290a19aae5948955bdd
Kevin Wenner <kwenner@impinj.com>
2021-03-12 15:59:56 -0800

    updated toolchain file for Impinj R700 ETK version 7.5.0

--- pnpbridge/src/adapters/src/impinj_reader_r700/support_files/toolchainfile.cmake
+++ pnpbridge/src/adapters/src/impinj_reader_r700/support_files/toolchainfile.cmake
@@ -3,11 +3,11 @@ include(CMakeForceCompiler)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7l)
 
-set(CMAKE_SYSROOT /home/kwenner/arm-toolchains/r700/7.3.0_Octane_Embedded_Development_Tools/arm-toolchain/arm-buildroot-linux-gnueabihf/sysroot)
+set(CMAKE_SYSROOT /home/kwenner/impinj/etk/r700/7.5.0_Octane_Embedded_Development_Tools/arm-toolchain/arm-buildroot-linux-gnueabihf/sysroot)
 
-set(tools /home/kwenner/arm-toolchains/r700/7.3.0_Octane_Embedded_Development_Tools/arm-toolchain)
-set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-c++)
+set(tools /home/kwenner/impinj/etk/r700/7.5.0_Octane_Embedded_Development_Tools/arm-toolchain)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-none-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-none-linux-gnueabihf-c++)
 
 set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} ${tools}/lib)
 
