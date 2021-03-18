1c736f74c9e3d17934c718f5b2df9c5c0afeec94
Kevin Wenner <kwenner@impinj.com>
2021-03-18 14:59:18 -0700

    updated build-r700.sh script to also build CAP via makefile

--- pnpbridge/scripts/linux/targets/impinj/build-r700.sh
+++ pnpbridge/scripts/linux/targets/impinj/build-r700.sh
@@ -1,1 +1,4 @@
-../../build.sh --toolchain-file ../../../../src/adapters/src/impinj_reader_r700/support_files/toolchainfile.cmake
+../../build.sh --toolchain-file ../../../../src/adapters/src/impinj_reader_r700/support_files/toolchainfile.cmake && \
+cd ../../../../src/adapters/src/impinj_reader_r700/cap && \
+make clean && \
+make
