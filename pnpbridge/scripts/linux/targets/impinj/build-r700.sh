693cf169e83d0af40572ed772e87fc1afb0034d6
Kevin Wenner <kwenner@impinj.com>
2021-09-30 22:31:57 -0700

    updated build scripts, update to UPGX v0.1.14.3

--- pnpbridge/scripts/linux/targets/impinj/build-r700.sh
+++ pnpbridge/scripts/linux/targets/impinj/build-r700.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo SCRIPT_DIR: ${SCRIPT_DIR}
 
-while getopts bdu flag
+while getopts "bdu" flag
 do 
     case "${flag}" in 
         b) noBaseBuild="TRUE";;
@@ -11,18 +11,22 @@ do
 done
 
 if [ "${noBaseBuild}" != "TRUE" ]; then
+    echo Building Binary....
     ${SCRIPT_DIR}/../../build.sh --toolchain-file ${SCRIPT_DIR}/../../../../src/adapters/src/impinj_reader_r700/support_files/toolchainfile.cmake
     cd ${SCRIPT_DIR}/../../../../src/adapters/src/impinj_reader_r700/cap
     make clean
     make
 fi
 
-if [ "${dockerBuild}" != "TRUE" ]; then
+if [ "${noDockerBuild}" != "TRUE" ]; then
+    echo Building Docker Image...
     cd ${SCRIPT_DIR}/../../../../../pnpbridge/src/adapters/src/impinj_reader_r700/docker
     ./build-docker-image.sh
     cd ${SCRIPT_DIR}
 fi
 
-if [ "${upgxBuild}" != "TRUE" ]; then
-    ${SCRIPT_DIR}/../../../../../pnpbridge/src/adapters/src/impinj_reader_r700/support_files/build_upgx_customconfig.sh
+if [ "${noUpgxBuild}" != "TRUE" ]; then
+    echo Building UPGX...
+    cd ${SCRIPT_DIR}/../../../../../pnpbridge/src/adapters/src/impinj_reader_r700/support_files
+    ./build_upgx_customconfig.sh
 fi
